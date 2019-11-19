63bcf606d85660ce1cc3071d31931e0e9150cc54
ravivumule <ravignt2@gmail.com>
2019-11-19 20:07:24 +0530

    my secod commit

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+//
 
 app.use('/api/v1/customer', customer);
 app.use('/api/v1/generate_uid', generate_uid);
