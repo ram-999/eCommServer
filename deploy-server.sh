b2df55a4e8ab8c77cd6aea839b42aa6a23feb783
ravivumule <ravignt2@gmail.com>
2019-11-19 18:22:27 +0530

    My First server changes

--- deploy-server.sh
+++ deploy-server.sh
@@ -1,4 +1,4 @@
 docker build --no-cache -t ecomm-server .
 docker stop ecomm-server-container
 docker rm ecomm-server-container
-docker run -p 4200:80 -d -it  --name ecomm-server-container ecomm-server
+docker run -p 3000:3000 -d -it  --name ecomm-server-container ecomm-server
